e28d308b9943f0e7a6785d65943b04517af62af6
Wangutusi Arshton <wangarshton@gmail.com>
2024-05-18 20:31:12 +0300

    removed search feature

--- sendemail.php
+++ sendemail.php
@@ -1,24 +1,41 @@
 <?php
-// Get data from form
-$name    = $_POST['name'];
-$email   = $_POST['email'];
-$message = $_POST['message'];
+if (isset($_POST['submit'])) {
+    $name    = $_POST['name'];
+    $email   = $_POST['email'];
+    $ema     = "from . $email";
+    $mes     = "from . $message";
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$to      = "wangarshton@gmail.com";
-$subject = "From wanglayouts.com";
+    $receiver = "wangarshton@gmail.com";
+    $subject  = $subj;
+    $message  = $name . '<br>' . $mes;
+    $header   = $ema;
+    $header .= "MIME-Version:1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
-$txt = "Name = " . $name . "\r\n  Email = "
-    . $email . "\r\n Message =" . $message;
+    $test = mail($receiver, $subject, $message, $header);
+    if ($test == true) {
+        header("Location:index.php");
+    } else {
+        echo 'incorrect email address';
+    }
+    ;
 
-$headers = "From: noreply@demosite.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
-if ($email != null) {
-    mail($to, $subject, $txt, $headers);
 }
+;
 
-// Redirect to
-header("Location:contact.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
